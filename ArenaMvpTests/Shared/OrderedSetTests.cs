c811460b86f6ae1d1afdbbc7f36e5077ff558f79
Damjan Dabovski <dad@proofficepark.dk>
2023-09-17 19:53:28 +0200

    Adding OrderedSet and tests for it

---
+++ ArenaMvpTests/Shared/OrderedSetTests.cs
@@ -0,0 +1,123 @@
+﻿namespace ArenaMvpTests.Shared
+{
+    using NeonArenaMvp.Shared;
+
+    [TestClass]
+    public class OrderedSetTests
+    {
+        private readonly OrderedSet<int> orderedSet;
+
+        public OrderedSetTests()
+        {
+            orderedSet = new();
+        }
+
+        [TestMethod]
+        public void AddsMultipleItems()
+        {
+            // Act
+            orderedSet.Add(1);
+            orderedSet.Add(2);
+
+            // Assert
+            Assert.AreEqual(2, orderedSet.Count);
+        }
+
+        [TestMethod]
+        public void DoesntAddAnExistingItem()
+        {
+            // Arrange
+            orderedSet.Add(1);
+
+            // Act
+            var isAddSuccessful = orderedSet.Add(1);
+
+            // Assert
+            Assert.IsFalse(isAddSuccessful);
+            Assert.AreEqual(1, orderedSet.Count);
+        }
+
+        [TestMethod]
+        public void PreservesOrderWhenAdding()
+        {
+            // Act
+            orderedSet.Add(1);
+            orderedSet.Add(2);
+            orderedSet.Add(3);
+
+            // Assert
+            var expectedOrder = new int[] { 1, 2, 3 };
+            var isOrderPreserved = true;
+            var loopIndex = 0;
+
+            foreach (var item in orderedSet)
+            {
+                if (item != expectedOrder[loopIndex])
+                {
+                    isOrderPreserved = false;
+                }
+                loopIndex++;
+            }
+
+            Assert.IsTrue(isOrderPreserved);
+        }
+
+        [TestMethod]
+        public void RemovesAnItem()
+        {
+            // Arrange
+            orderedSet.Add(1);
+            orderedSet.Add(2);
+
+            // Act
+            var isRemoveSuccessful = orderedSet.Remove(2);
+
+            // Assert
+            Assert.IsTrue(isRemoveSuccessful);
+            Assert.AreEqual(1, orderedSet.Count);
+            Assert.IsFalse(orderedSet.Any(x => x == 2));
+        }
+
+        [TestMethod]
+        public void DoesntRemoveAnItemThatDoesntExist()
+        {
+            // Arrange
+            orderedSet.Add(1);
+
+            // Act
+            var isRemoveSuccessful = orderedSet.Remove(2);
+
+            // Assert
+            Assert.IsFalse(isRemoveSuccessful);
+            Assert.AreEqual(1, orderedSet.Count);
+        }
+
+        [TestMethod]
+        public void PreservesOrderWhenRemoving()
+        {
+            // Arrange
+            orderedSet.Add(1);
+            orderedSet.Add(2);
+            orderedSet.Add(3);
+
+            // Act
+            orderedSet.Remove(2);
+
+            // Assert
+            var expectedOrder = new int[] { 1, 3 };
+            var isOrderPreserved = true;
+            var loopIndex = 0;
+
+            foreach (var item in orderedSet)
+            {
+                if (item != expectedOrder[loopIndex])
+                {
+                    isOrderPreserved = false;
+                }
+                loopIndex++;
+            }
+
+            Assert.IsTrue(isOrderPreserved);
+        }
+    }
+}
---
+++ NeonArenaMvp/Shared/OrderedSet.cs
@@ -0,0 +1,81 @@
+﻿namespace NeonArenaMvp.Shared
+{
+    using System.Collections;
+
+    public class OrderedSet<T> : ICollection<T> where T: notnull
+    {
+        private readonly IDictionary<T, LinkedListNode<T>> dictionary;
+        private readonly LinkedList<T> linkedList;
+
+        public OrderedSet()
+            : this(EqualityComparer<T>.Default)
+        {
+        }
+
+        public OrderedSet(IEqualityComparer<T> comparer)
+        {
+            dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+            linkedList = new LinkedList<T>();
+        }
+
+        public int Count => dictionary.Count;
+
+        public virtual bool IsReadOnly => dictionary.IsReadOnly;
+
+        void ICollection<T>.Add(T item)
+        {
+            Add(item);
+        }
+
+        public void Clear()
+        {
+            linkedList.Clear();
+            dictionary.Clear();
+        }
+
+        public bool Remove(T item)
+        {
+            bool found = dictionary.TryGetValue(item, out LinkedListNode<T>? node);
+
+            if (!found || node is null)
+            {
+                return false;
+            }
+
+            dictionary.Remove(item);
+            linkedList.Remove(node);
+
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return linkedList.GetEnumerator();
+        }
+
+        public bool Contains(T item) => dictionary.ContainsKey(item);
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            linkedList.CopyTo(array, arrayIndex);
+        }
+
+        public bool Add(T item)
+        {
+            if (dictionary.ContainsKey(item))
+            {
+                return false;
+            }
+
+            LinkedListNode<T> node = linkedList.AddLast(item);
+            dictionary.Add(item, node);
+
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)linkedList).GetEnumerator();
+        }
+    }
+}
