ed25540ec07aff5f6c598451ee0f0da5bfe3caf7
Damjan Dabovski <dad@proofficepark.dk>
2023-10-06 18:42:58 +0200

    Changing Tile Sector dictionary to immutable

--- NeonArenaMvp/Game/Maps/Tile.cs
+++ NeonArenaMvp/Game/Maps/Tile.cs
@@ -2,6 +2,7 @@
 {
     using NeonArenaMvp.Game.Behaviors.Tile;
     using NeonArenaMvp.Game.Maps.Actions;
+    using System.Collections.Immutable;
     using static NeonArenaMvp.Game.Maps.Enums;
 
     public class Tile
@@ -9,20 +10,20 @@
     {
         public readonly Direction Direction;
 
-        private readonly Dictionary<Sector, TileBehavior> SectorBehaviors;
+        private readonly ImmutableDictionary<Sector, TileBehavior> SectorBehaviors;
 
         public Tile(Direction direction = Direction.Up, TileBehavior? centerBehavior = null, TileBehavior? upBehavior = null, TileBehavior? rightBehavior = null, TileBehavior? downBehavior = null, TileBehavior? leftBehavior = null)
         {
             this.Direction = direction;
 
-            this.SectorBehaviors = new()
+            this.SectorBehaviors = new Dictionary<Sector, TileBehavior>()
             {
                 [Sector.Center] = centerBehavior ?? TileBehaviors.Empty,
                 [Sector.Up] = upBehavior ?? TileBehaviors.Empty,
                 [Sector.Right] = rightBehavior ?? TileBehaviors.Empty,
                 [Sector.Down] = downBehavior ?? TileBehaviors.Empty,
                 [Sector.Left] = leftBehavior ?? TileBehaviors.Empty
-            };
+            }.ToImmutableDictionary();
         }
 
         public MoveAction? GetNextMove(MoveAction currentMoveAction)
