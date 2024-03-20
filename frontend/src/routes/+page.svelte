c58d265b6d6836c8b1c61b35c45ed3722204b7fc
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-03-20 19:20:40 +0100

    Add basic UI

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<div class="m-5 flex flex-col gap-2">
-	<h1 class="text-5xl text-cyan-700">Welcome to SvelteKit</h1>
-	<p>
-		Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-	</p>
+<div class="m-5 flex flex-col gap-20 items-center justify-center h-dvh">
+	<h1 class="text-7xl text-slate-600">Welcome to <span class="underline text-cyan-700">Neon Arena</span></h1>
+	<a
+		href="/lobby"
+		class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 text-xl rounded inline-flex items-center"
+	>
+		Join a Lobby
+		<img alt="right-chevron" src="/icons/chevron-right.svg" class="ml-1" />
+	</a>
 </div>
---
+++ frontend/src/routes/lobby/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+	const lobbies = [
+		{
+			name: 'Some Lobby',
+			description:
+				'Voluptate nisi magna ipsum ullamco do aliqua minim reprehenderit eiusmod id eu.',
+			createdAt: new Date(),
+			currentPlayers: 2,
+			maxPlayers: 10
+		},
+		{
+			name: 'Some Lobby',
+			description:
+				'Voluptate nisi magna ipsum ullamco do aliqua minim reprehenderit eiusmod id eu.',
+			createdAt: new Date(),
+			currentPlayers: 3,
+			maxPlayers: 10
+		},
+		{
+			name: 'Some Lobby',
+			description:
+				'Voluptate nisi magna ipsum ullamco do aliqua minim reprehenderit eiusmod id eu.',
+			createdAt: new Date(),
+			currentPlayers: 5,
+			maxPlayers: 10
+		},
+		{
+			name: 'Some Lobby',
+			description:
+				'Voluptate nisi magna ipsum ullamco do aliqua minim reprehenderit eiusmod id eu.',
+			createdAt: new Date(),
+			currentPlayers: 10,
+			maxPlayers: 10
+		},
+		{
+			name: 'Some Lobby',
+			description:
+				'Voluptate nisi magna ipsum ullamco do aliqua minim reprehenderit eiusmod id eu.',
+			createdAt: new Date(),
+			currentPlayers: 1,
+			maxPlayers: 10
+		}
+	];
+</script>
+
+<div class="flex flex-col m-5">
+	<h1 class="text-5xl text-bold text-slate-600">Welcome to the Lobby</h1>
+	<p class="text-slate-600 text-xl">Pick and join a lobby bellow</p>
+	<ul class="flex flex-col gap-5 mt-10">
+		{#each lobbies as { name, description, createdAt, currentPlayers, maxPlayers }}
+			<li
+				class="px-5 py-3 shadow-md bg-slate-50 hover:bg-slate-100 hover:cursor-pointer hover:shadow-lg w-fit rounded flex flex-row gap-4"
+			>
+				<div class="flex flex-col gap-1">
+					<div class="flex gap-2 items-center">
+						<div class="text-slate-700">
+							{currentPlayers}/{maxPlayers}
+						</div>
+						<div class="p-1 bg-slate-700 rounded-full"></div>
+						<div class="text-xl text-slate-700">{name}</div>
+						{#if currentPlayers >= maxPlayers}
+							<div class="text-red-400">FULL</div>
+						{/if}
+					</div>
+					<div class="text-sm text-slate-500">{createdAt.toTimeString()}</div>
+					<div class="text-md text-slate-500 w-96">{description}</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</div>
