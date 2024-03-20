81157c69ab1b24bb9dda030cf1d7ff9c110c10a5
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-03-20 18:28:53 +0100

    Add tailwind

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-5 flex flex-col gap-2">
+	<h1 class="text-5xl text-cyan-700">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
